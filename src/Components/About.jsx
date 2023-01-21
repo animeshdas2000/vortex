e2c5b4ff3f6eef95b087e9cd2b9c6dbcbb2d9887
Animesh Das <animeshxdas@gmail.com>
2023-01-21 22:04:09 +0530

    minor correction

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,16 +3,12 @@ import React from "react";
 function About() {
   return (
     <div className="about-us" id="about">
-      <h1>About Us</h1>
+      <h1>About</h1>
 
       <p>
-        Vortex is a Cryptocurrency tracker. It allows you to keep track of how
-        the value of your coins are changing. You can also use the graphs for
-        visualising the data.
-      </p>
-      <p>
-        Google Authentication is enabled for ease of the users. We look forward
-        to add more features in the feautre.
+        Vortex is a Cryptocurrency tracker. It allows you to keep track of
+        cryptocurrency prices. You can also use the graphs for visualising the
+        data.
       </p>
       <p>
         It is an Open source project. You can check it here on{" "}
--- src/Components/Dashboard/Content.jsx
+++ src/Components/Dashboard/Content.jsx
@@ -1,28 +1,42 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import Bitcoin from "../../assets/Bitcoin-pana.png"
+import Bitcoin from "../../assets/Bitcoin-pana.png";
 import { UserContext } from "../../context/userContext";
 import { GoogleAuth } from "../../services/Auth";
 
-
 function Content() {
-  const {user} = useContext(UserContext) 
-  const contentStyle = {  padding: "5%",display:"flex",flexDirection:"row",alignItems:"center",flexWrap:"wrap",justifyContent:"space-between" };
+  const { user } = useContext(UserContext);
+  const contentStyle = {
+    padding: "2% 10%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+  };
   return (
     <div style={contentStyle}>
       <div>
-        <h1 style={{width:"fit-content",margin:"0"}}>The Best way to <br /> Track your Crypto Currency</h1>
-        {
-          user?(<>
-          
-          <button className="cta-btn"><Link to="/bitcoin">Go to Dashboard</Link></button>
-          </>):(
-            <button className="cta-btn" onClick={()=>{GoogleAuth()}}>Get Started</button>
-          )
-        }
+        <h1 style={{ width: "fit-content", margin: "0" }}>
+          The Best way to <br /> Track your Crypto Currency
+        </h1>
+        {user ? (
+          <>
+            <button className="cta-btn">
+              <Link to="/bitcoin">Go to Dashboard</Link>
+            </button>
+          </>
+        ) : (
+          <button
+            className="cta-btn"
+            onClick={() => {
+              GoogleAuth();
+            }}>
+            Get Started
+          </button>
+        )}
       </div>
-      <img src={Bitcoin} width={300} alt="" />
-      
+      <img src={Bitcoin} width={400} alt="" />
     </div>
   );
 }
